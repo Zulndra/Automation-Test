f2e073f833ed9c30d14f6a8c7fc1c1ce23df918e
zulndra <ahmadinozulendra@gmail.com>
2026-01-14 11:22:19 +0700

    feat: Threads Discord

--- test.js
+++ test.js
@@ -17,7 +17,7 @@ function assert(condition, testName) {
 // Test 1: Check if app.js exists
 const fs = require('fs');
 assert(
-  fs.existsSync('app.js'), 
+  fs.existsSync('appdawdaw.js'), 
   'app.js file exists'
 );
 
